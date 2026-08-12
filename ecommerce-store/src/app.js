95eb33a7e8501b17968bc559e8191ec492c25ec8
Khaled Khttab <KHALEDKHTTAB1@GMAIL.COM>
2026-08-12 19:25:38 +0300

    setup Node.js, Express.js, Prisma and PostgreSQL

---
+++ ecommerce-store/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("E-Commerce API is running!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
